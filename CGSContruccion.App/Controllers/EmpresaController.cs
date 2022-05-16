1975ad8591b3cd4a175a74b5430c553ccd3a7eb9
oscar.castillo <oscar.castillo@LPTOCASTILLO>
2022-05-16 15:39:24 -0500

    Vistas Empresa

---
+++ CGSContruccion.App/Controllers/EmpresaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CGSContruccion.App.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        // GET: Empresa
+        public ActionResult Empresa()
+        {
+            return View();
+        }
+
+        public ActionResult Crear_Empresa()
+        {
+            return View();
+        }
+
+        public ActionResult Editar_Empresa()
+        {
+            return View();
+        }
+    }
+}
---
+++ CGSContruccion.App/Controllers/SucursalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CGSContruccion.App.Controllers
+{
+    public class SucursalController : Controller
+    {
+        // GET: Sucursal
+        public ActionResult Sucursal()
+        {
+            return View();
+        }
+    }
+}
